79600d5b6172d016cd327002ebd8184a236f4f0c
Do Lam Binh Minh <minhdlb@blockchainlabs.asia>
2021-03-31 13:43:24 +0700

    Store session in Redis database

--- src/index.js
+++ src/index.js
@@ -1,15 +1,30 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import session from 'express-session';
+import redis from 'redis';
+import RedisStore from 'connect-redis';
 import passport from "passport";
 import facebookRouter from './routes/facebook.route';
 import dotenv from 'dotenv';
 
 dotenv.config();
 const PORT = process.env.PORT || 8888;
+const REDIS_HOST = process.env.REDIS_HOST || '127.0.0.1';
+const REDIS_PORT = process.env.REDIS_PORT || 6379;
+const REDIS_PASSWORD = process.env.REDIS_PASSWORD || 'i dont know';
+
+const redisStore = RedisStore(session);
+const redisClient = redis.createClient({ host: '127.0.0.1', port: 6379, password: 'ocsen-hoc-code' });
 
 const app = express();
-app.use(session({ secret: "OcSen Hoc Code" }));
+app.use(session({
+    secret: "OcSen Hoc Code",
+    store: new redisStore({
+        client: redisClient
+    }),
+    cookie: { maxAge: 60000 }
+}));
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(passport.initialize());
 app.use(passport.session());
